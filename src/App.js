d2f4a6bde35218a29768691eaa9d8584799ce9e5
duchieu <duchieu.ctk41@gmail.com>
2020-12-20 18:17:06 +0700

    [1.0.0] add eject.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import classes from "./App.modules.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className={classes.App}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
