eee1d5648e6e0e92f887a8496e7ced7eb8765d56
duchieu <duchieu.ctk41@gmail.com>
2020-12-22 10:56:00 +0700

    [1.0.0] add course.

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 
-const courses = () => (
-    <h2>Courses</h2>
-);
+class Courses extends Component {
+  state = {
+    course: {
+      title: "",
+    },
+  };
 
-export default courses;
+  handleChange = (event) => {
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({ course });
+  };
+
+  render() {
+    return (
+      <form>
+        <h2>Courses</h2>
+        <h3>Add Courses</h3>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.course.title}
+        />
+        <input type="submit" value="Save" />
+      </form>
+    );
+  }
+}
+
+export default Courses;
